bd8543109c36043ff3adc8f2e37f8b4e7d76d578
David Arango <davidarango82@gmail.com>
2018-02-12 17:46:18 -0500

    my second commit

--- index.js
+++ index.js
@@ -4,18 +4,22 @@
 };*/
 var rect = require('./rectangle');
 
-function solveRect(l,b){
-  console.log("Solving for rect with l = " + l + " and b = " + b);
-  
-  if (l <= 0 || b <= 0){
-    console.log("invalid dimensions");
-  }
-  
-  else{
-    console.log("the area is " + rect.area(l,b));
-    console.log("the perimeter is " + rect.perimeter(l,b));
-  }
-}
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
 
 solveRect(2,2);
 solveRect(3,0);
